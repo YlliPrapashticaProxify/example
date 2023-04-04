5ec84efc4f2e6dbde4e4e16cf27e7062475f9161
Ivan Hofer <ivan.hofer@outlook.com>
2023-04-04 12:23:36 +0200

    a

--- _plugin.js
+++ _plugin.js
@@ -1,3 +1,137 @@
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __commonJS = (cb, mod) => function __require() {
+  return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
+
+// node_modules/flat/index.js
+var require_flat = __commonJS({
+  "node_modules/flat/index.js"(exports, module) {
+    module.exports = flatten2;
+    flatten2.flatten = flatten2;
+    flatten2.unflatten = unflatten;
+    function isBuffer(obj) {
+      return obj && obj.constructor && typeof obj.constructor.isBuffer === "function" && obj.constructor.isBuffer(obj);
+    }
+    function keyIdentity(key) {
+      return key;
+    }
+    function flatten2(target, opts) {
+      opts = opts || {};
+      const delimiter = opts.delimiter || ".";
+      const maxDepth = opts.maxDepth;
+      const transformKey = opts.transformKey || keyIdentity;
+      const output = {};
+      function step(object, prev, currentDepth) {
+        currentDepth = currentDepth || 1;
+        Object.keys(object).forEach(function (key) {
+          const value = object[key];
+          const isarray = opts.safe && Array.isArray(value);
+          const type = Object.prototype.toString.call(value);
+          const isbuffer = isBuffer(value);
+          const isobject = type === "[object Object]" || type === "[object Array]";
+          const newKey = prev ? prev + delimiter + transformKey(key) : transformKey(key);
+          if (!isarray && !isbuffer && isobject && Object.keys(value).length && (!opts.maxDepth || currentDepth < maxDepth)) {
+            return step(value, newKey, currentDepth + 1);
+          }
+          output[newKey] = value;
+        });
+      }
+      step(target);
+      return output;
+    }
+    function unflatten(target, opts) {
+      opts = opts || {};
+      const delimiter = opts.delimiter || ".";
+      const overwrite = opts.overwrite || false;
+      const transformKey = opts.transformKey || keyIdentity;
+      const result = {};
+      const isbuffer = isBuffer(target);
+      if (isbuffer || Object.prototype.toString.call(target) !== "[object Object]") {
+        return target;
+      }
+      function getkey(key) {
+        const parsedKey = Number(key);
+        return isNaN(parsedKey) || key.indexOf(".") !== -1 || opts.object ? key : parsedKey;
+      }
+      function addKeys(keyPrefix, recipient, target2) {
+        return Object.keys(target2).reduce(function (result2, key) {
+          result2[keyPrefix + delimiter + key] = target2[key];
+          return result2;
+        }, recipient);
+      }
+      function isEmpty(val) {
+        const type = Object.prototype.toString.call(val);
+        const isArray = type === "[object Array]";
+        const isObject = type === "[object Object]";
+        if (!val) {
+          return true;
+        } else if (isArray) {
+          return !val.length;
+        } else if (isObject) {
+          return !Object.keys(val).length;
+        }
+      }
+      target = Object.keys(target).reduce(function (result2, key) {
+        const type = Object.prototype.toString.call(target[key]);
+        const isObject = type === "[object Object]" || type === "[object Array]";
+        if (!isObject || isEmpty(target[key])) {
+          result2[key] = target[key];
+          return result2;
+        } else {
+          return addKeys(
+            key,
+            result2,
+            flatten2(target[key], opts)
+          );
+        }
+      }, {});
+      Object.keys(target).forEach(function (key) {
+        const split = key.split(delimiter).map(transformKey);
+        let key1 = getkey(split.shift());
+        let key2 = getkey(split[0]);
+        let recipient = result;
+        while (key2 !== void 0) {
+          if (key1 === "__proto__") {
+            return;
+          }
+          const type = Object.prototype.toString.call(recipient[key1]);
+          const isobject = type === "[object Object]" || type === "[object Array]";
+          if (!overwrite && !isobject && typeof recipient[key1] !== "undefined") {
+            return;
+          }
+          if (overwrite && !isobject || !overwrite && recipient[key1] == null) {
+            recipient[key1] = typeof key2 === "number" && !opts.object ? [] : {};
+          }
+          recipient = recipient[key1];
+          if (split.length > 0) {
+            key1 = getkey(split.shift());
+            key2 = getkey(split[0]);
+          }
+        }
+        recipient[key1] = unflatten(target[key], opts);
+      });
+      return result;
+    }
+  }
+});
+
 // node_modules/just-safe-set/index.mjs
 var objectSafeSet = set;
 function set(obj, propsArg, value) {
@@ -40,6 +174,7 @@ function prototypeCheck(prop) {
 }
 
 // src/index.ts
+var import_flat = __toESM(require_flat(), 1);
 console.log(0, "hello from plugin");
 async function getLanguages(args) {
   const [pathBeforeLanguage, pathAfterLanguage] = args.pluginConfig.pathPattern.split("{language}");
@@ -60,9 +195,8 @@ async function readResources(args) {
       "{language}",
       language
     );
-    const flatJson = flatten(
-      JSON.parse(await args.$fs.readFile(resourcePath, "utf-8"))
-    );
+    const json = JSON.parse(await args.$fs.readFile(resourcePath, "utf-8"));
+    const flatJson = (0, import_flat.default)(json);
     result.push(parseResource(flatJson, language));
   }
   return result;
@@ -101,7 +235,7 @@ function parseMessage(id, value) {
 function serializeResource(resource) {
   console.log(1, resource);
   const obj = {};
-  const flatObject = resource.body.forEach((message) => {
+  resource.body.forEach((message) => {
     objectSafeSet(obj, message.id.name, serializeMessage(message));
   });
   console.log(2, obj);
